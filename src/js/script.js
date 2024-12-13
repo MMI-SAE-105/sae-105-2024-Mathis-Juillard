936d0d220af02397183d3a3f21cb47cde35b5ba7
Mathis-Juillard <mathis.juillard@edu.univ-fcomte.fr>
2024-12-13 11:14:09 +0100

    menu modifier

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,19 @@
+// On cible les éléments à modifier
+const toggle = document.querySelector(".menu-btn");
+const nav = document.querySelector(".menu");
+const page = document.body;
+
+// Vérifier si les éléments existent avant d'ajouter l'événement
+if (toggle && nav) {
+  toggle.addEventListener("click", () => {
+    const isOpen = toggle.ariaExpanded === "true";
+    const isClosed = !isOpen;
+    // Mise à jour des attributs ARIA pour accessibilité
+    toggle.ariaExpanded = isClosed;
+    nav.ariaHidden = isOpen;
+    page.classList.toggle("noscroll", isClosed);
+  });
+}
 // *** CAROUSEL ***
 // TODO
 const carousel = document.querySelector(".carousel__container");
@@ -19,20 +35,5 @@ if (carousel) {
     carousel.scrollBy({ left: +scrollAmount, behavior: "smooth" });;
   });
 }
-///////////////////
-// On cible les éléments à modifier
-const toggle = document.querySelector(".menu-btn");
-const nav = document.querySelector(".menu");
-const page = document.body;
 
-// Vérifier si les éléments existent avant d'ajouter l'événement
-if (toggle && nav) {
-  toggle.addEventListener("click", () => {
-    const isOpen = toggle.ariaExpanded === "true";
-    const isClosed = !isOpen;
-    // Mise à jour des attributs ARIA pour accessibilité
-    toggle.ariaExpanded = isClosed;
-    nav.ariaHidden = isOpen;
-    page.classList.toggle("noscroll", isClosed);
-  });
-}
+
